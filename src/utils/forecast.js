45211c4ccdac24ed7ecbf5a0d48501bf40609098
kanishths <kanishthasiyaram15@gmail.com>
2021-07-22 21:14:32 +0530

    api mask

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,8 +1,11 @@
 const request = require("request");
-
+const { accessKey } = require("../config");
+console.log("key:", accessKey);
 const forecast = (lat, lon, callback) => {
   const url =
-    "http://api.weatherstack.com/current?access_key=e9f4afcbceff742864b305f76dc41282&query=" +
+    "http://api.weatherstack.com/current?access_key=" +
+    accessKey +
+    "&query=" +
     lat +
     "," +
     lon +
@@ -14,9 +17,28 @@ const forecast = (lat, lon, callback) => {
     } else if (body.error) {
       callback("Unable to find Location", undefined);
     } else {
+      const arr = [
+        "",
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ];
+      const year = body.location.localtime.substring(0, 4);
+      const month = parseInt(body.location.localtime.substring(5, 7));
+      const date = body.location.localtime.substring(8, 10);
+      const time = body.location.localtime.substring(11);
       callback(
         undefined,
-        `The Temperature at ${body.location.localtime} is ${body.current.temperature}\u00B0C out and it Feels Like ${body.current.feelslike}\u00B0C.`
+        `The Temperature at ${arr[month]} ${date}, ${year} ${time},  is ${body.current.temperature}\u00B0C out and it Feels Like ${body.current.feelslike}\u00B0C. The humidity outside is ${body.current.humidity}%`
       );
     }
   });
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,10 +1,12 @@
 const request = require("request");
-
+const { accessToken } = require("../config");
+console.log("token:", accessToken);
 const geocode = (address, callback) => {
   const url =
     "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
     encodeURIComponent(address) +
-    ".json?access_token=pk.eyJ1Ijoia2FuaXNodGhzIiwiYSI6ImNrcmF1OW11czBycnIzMnF1YjF0bDIwZ3IifQ.U7ztLu18We5ZIyp-2Hsyug";
+    ".json?access_token=" +
+    accessToken;
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
